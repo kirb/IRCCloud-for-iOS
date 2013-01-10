3b41a2eb538d79dbf3ffa835d25f2f8322c6eb09
caughtinflux <caughtinflux@icloud.com>
2013-01-10 11:30:39 +0530

    Add empty ICBuffer subclass for private messages

---
+++ IRCCloud/ICConversation.h
@@ -0,0 +1,13 @@
+//
+//  ICConversation.h
+//  IRCCloud
+//
+//  Created by Aditya KD on 10/01/13.
+//  Copyright (c) 2013 HASHBANG Productions. All rights reserved.
+//
+
+#import "ICBuffer.h"
+
+@interface ICConversation : ICBuffer
+
+@end
---
+++ IRCCloud/ICConversation.m
@@ -0,0 +1,13 @@
+//
+//  ICConversation.m
+//  IRCCloud
+//
+//  Created by Aditya KD on 10/01/13.
+//  Copyright (c) 2013 HASHBANG Productions. All rights reserved.
+//
+
+#import "ICConversation.h"
+
+@implementation ICConversation
+
+@end
